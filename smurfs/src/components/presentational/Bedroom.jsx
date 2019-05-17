587f640fe3498890433db54ca0960d62dc502a8e
Moses Samuel <mosamuel98@gmail.com>
2019-05-17 10:27:25 -0700

    fix: change for to htmlFor in jsx + stop referencing state

--- smurfs/src/components/presentational/Bedroom.jsx
+++ smurfs/src/components/presentational/Bedroom.jsx
@@ -2,49 +2,51 @@ import React, { useState } from 'react'
 
 
 export default function Bedroom(props) {
-    const [newVillager, setNewVillager] = useState({
-        name: '',
-        age: '',
-        height: ''
-    })
+    // const [newVillager, setNewVillager] = useState({
+    //     name: '',
+    //     age: '',
+    //     height: ''
+    // })
 
-    const handleChange = (e) => {
-        setNewVillager({
-            ...newVillager
-            [e.target.id] = newVillager[e.target.value]
-        }) 
-    }
+    // const handleChange = (e) => {
+    //     setNewVillager({
+    //         ...newVillager
+    //         [e.target.id] = newVillager[e.target.value]
+    //     }) 
+    // }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        props.addVillager(newVillager)
-    }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     props.addVillager(newVillager)
+    // }
+
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label for="name">Name</label>
+        // <form onSubmit={handleSubmit}>
+        <form>
+            <label htmlFor="name">Name</label>
             <input 
                 id="name"
                 type="text" 
                 placeholder="enter smurf name"
-                value={newVillager}
-                onChange={handleChange}
+                // value={newVillager}
+                // onChange={handleChange}
             />
-            <label for="age">Age</label>
+            <label htmlFor="age">Age</label>
             <input 
                 id="age"
                 type="text" 
                 placeholder="enter smurf age"
-                value={newVillager}
-                onChange={handleChange}
+                // value={newVillager}
+                // onChange={handleChange}
             />
-            <label for="height">Height</label>
+            <label htmlFor="height">Height</label>
             <input 
                 id="height"
                 type="text" 
                 placeholder="enter smurf height"
-                value={newVillager}
-                onChange={handleChange}
+                // value={newVillager}
+                // onChange={handleChange}
             />
         </form>
     )
