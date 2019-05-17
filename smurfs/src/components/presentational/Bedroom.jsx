a50da1c3115a8fc397b5ed0a1250b1e1229e514e
Moses Samuel <mosamuel98@gmail.com>
2019-05-17 10:07:30 -0700

    add Bedroom presentational component

---
+++ smurfs/src/components/presentational/Bedroom.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+export default function Bedroom(props) {
+    const [newVillager, setNewVillager] = useState({
+        name: '',
+        age: '',
+        height: ''
+    })
+
+    const handleChange = (e) => {
+        setNewVillager({
+            ...newVillager
+            [e.target.id] = newVillager[e.target.value]
+        }) 
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addVillager(newVillager)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label for="name">Name</label>
+            <input 
+                id="name"
+                type="text" 
+                placeholder="enter smurf name"
+                value={newVillage}
+                onChange={handleChange}
+            />
+            <label for="age">Age</label>
+            <input 
+                id="age"
+                type="text" 
+                placeholder="enter smurf age"
+                value={newVillage}
+                onChange={handleChange}
+            />
+            <label for="height">Height</label>
+            <input 
+                id="height"
+                type="text" 
+                placeholder="enter smurf height"
+                value={newVillage}
+                onChange={handleChange}
+            />
+        </form>
+    )
+}
